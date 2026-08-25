6cd876095da52f563c4fdec95933bf62b2d31789
Exhen <exhen32@live.com>
2026-08-25 14:38:11 +0800

    feat: prefetch

--- wereadlite.koplugin/wereadlite/config.lua
+++ wereadlite.koplugin/wereadlite/config.lua
@@ -1,6 +1,6 @@
 local C = {
     NAME = "微信读书",
-    VERSION = "0.1.1",
+    VERSION = "0.2.0",
     PLUGIN_DIR = "wereadlite.koplugin",
     SETTINGS_FILE = "wereadlite.lua",
     ENTRY_FILENAME = "微信读书.epub",
